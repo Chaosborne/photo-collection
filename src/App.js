ea9ac87ed5af64b8c3ca14c0b6867ef7d61219b4
Chaosborne <rick.chaosborne@gmail.com>
2024-04-17 17:03:30 +0300

    category selection buttons with dynamic layout and highlight active button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Collection } from "./Collection";
 import "./index.scss";
 
+const categories = [{ name: "All" }, { name: "Sea" }, { name: "Mountains" }, { name: "Architecture" }, { name: "Cities" }];
+
 function App() {
   const [searchValue, setSeacrhValue] = useState("");
   const [collections, setCollections] = useState([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState(0);
 
   useEffect(() => {
     fetch("https://661e9e3716358961cd92650a.mockapi.io/photo-collections")
@@ -23,11 +26,17 @@ function App() {
       <h1>My photo collection</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">All</li>
-          <li>Mountains</li>
-          <li>Sea</li>
-          <li>Architecture</li>
-          <li>Cities</li>
+          {categories.map((cat, i) => (
+            <li
+              onClick={() => {
+                setSelectedCategoryId(i);
+              }}
+              className={selectedCategoryId === i ? "active" : ""}
+              key={cat.name}
+            >
+              {cat.name}
+            </li>
+          ))}
         </ul>
         <input onChange={(e) => setSeacrhValue(e.target.value)} value={searchValue} className="search-input" placeholder="Search by name" />
       </div>
